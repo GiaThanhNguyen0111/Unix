c3aecd3d904dfc97a03696401e117b9304e5ab80
GiaThanhNguyen0111 <lotumo123@gmail.com>
2023-08-09 23:09:49 +0700

    End today's work hahaha

--- index.php
+++ index.php
@@ -1,50 +1,52 @@
 <?php
 
 require __DIR__ . "/config/bootstrap.php";
+require PROJECT_ROOT_PATH . "/controller/api/ProductController.php";
+require PROJECT_ROOT_PATH . "/controller/api/CustomerController.php";
+require PROJECT_ROOT_PATH . "/controller/api/OrderController.php";
+require PROJECT_ROOT_PATH . "/controller/api/StoreController.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode( '/', $uri );
 
-if ((isset($uri[2]) && $uri[2] != 'customer') || !isset($uri[3])) {
+if (!(isset($uri[2]))) {
 
     header("HTTP/1.1 404 Not Found");
 
     exit();
-
 }
 
-require PROJECT_ROOT_PATH . "/controller/api/CustomerController.php";
+if (isset($uri[2]) == "customer") {
+    // CustomerController
+    $objFeedController = new CustomerController();
+    
+    $strMethodName = $uri[3] . 'Action';
+    
+    $objFeedController->{$strMethodName}();
+} elseif (isset($uri[2]) == "order") {
+    // Order Controller
+    $objFeedController = new OrderController();
+    
+    $strMethodName = $uri[3] . 'Action';
+    
+    $objFeedController->{$strMethodName}();
+} elseif (isset($uri[2]) == "product") {
+    // Product Controller
+    $objFeedController = new ProductController();
+    
+    $strMethodName = $uri[3] . 'Action';
+    
+    $objFeedController->{$strMethodName}();
+} elseif (isset($uri[2]) == "store") {
+    //Store Controller
+    $objFeedController = new StoreController();
+    
+    $strMethodName = $uri[3] . 'Action';
+    
+    $objFeedController->{$strMethodName}();
+}
 
-$objFeedController = new CustomerController();
-
-$strMethodName = $uri[3] . 'Action';
-
-$objFeedController->{$strMethodName}();
-
-require PROJECT_ROOT_PATH . "/controller/api/OrderController.php";
-
-$objFeedController = new OrderController();
-
-$strMethodName = $uri[3] . 'Action';
-
-$objFeedController->{$strMethodName}();
-
-require PROJECT_ROOT_PATH . "/controller/api/ProductController.php";
-
-$objFeedController = new ProductController();
-
-$strMethodName = $uri[3] . 'Action';
-
-$objFeedController->{$strMethodName}();
-
-require PROJECT_ROOT_PATH . "/controller/api/StoreController.php";
-
-$objFeedController = new StoreController();
-
-$strMethodName = $uri[3] . 'Action';
-
-$objFeedController->{$strMethodName}();
 
 ?>
 
